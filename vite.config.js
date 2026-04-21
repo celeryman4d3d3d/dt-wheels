2dc8a079abf53638fcdd4b93960289f26cab9f61
Cameron Buckley <cameron.buckley@protonmail.com>
2026-04-20 21:15:02 -0700

    Updated vite

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  server: {
+    allowedHosts: ['devserver-preview--dt-wheels.netlify.app'],
+  },
 })
